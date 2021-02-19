92112046eec1456b5f19a445cf3c107f76f4cd92
YoungKyonYou <man.spride1324@gmail.com>
2021-02-19 10:38:29 +0900

    initial commit

---
+++ src/main/java/org/morgorithm/frames/entity/MemberImage.java
@@ -0,0 +1,26 @@
+package org.morgorithm.frames.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString(exclude="member")
+public class MemberImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inum;
+
+    private String uuid;
+
+    private String imgName;
+
+    private String path;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    private Member member;
+}
