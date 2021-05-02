e92ac2b84fe4961b33d3aa5801da0387d50ab7c6
YoungKyonYou <man.spride1324@gmail.com>
2021-05-02 15:23:54 +0900

    feat: add query mthod for fetching facilities names

--- src/main/java/org/morgorithm/frames/repository/FacilityRepository.java
+++ src/main/java/org/morgorithm/frames/repository/FacilityRepository.java
@@ -4,8 +4,11 @@ import org.morgorithm.frames.entity.Facility;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface FacilityRepository extends JpaRepository<Facility,Long> {
     @Query("select count(f) from Facility f")
     int getFacilityNum();
-
+    @Query("select f.building from Facility f")
+    List<Object[]> getFacilityNames();
 }
