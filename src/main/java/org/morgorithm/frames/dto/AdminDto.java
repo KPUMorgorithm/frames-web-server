ff1023fc7fb9f4ae767dc2524e33ac53489d65c6
outstandingboy <outstanding1301@gmail.com>
2021-05-03 15:43:06 +0900

    feat: entity Admin, dto AdminDto 추가

---
+++ src/main/java/org/morgorithm/frames/dto/AdminDto.java
@@ -0,0 +1,28 @@
+package org.morgorithm.frames.dto;
+
+import lombok.*;
+import org.morgorithm.frames.entity.Admin;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AdminDto {
+    private Long id;
+    private String username;
+    private String password;
+    private String passwordRpt;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public Admin toEntity() {
+        return Admin.builder()
+                .id(id)
+                .username(username)
+                .password(password)
+                .build();
+    }
+}
