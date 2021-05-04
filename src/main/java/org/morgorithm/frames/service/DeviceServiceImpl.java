b060386c770658271277831d0e3317f2c145d021
outstandingboy <outstanding1301@gmail.com>
2021-05-04 22:36:02 +0900

    feat: DeviceService 추가
    addFacility: 기기ID + 건물명으로 건물, 기기 생성 후 건물에 매핑
    addDevice: 기기ID + 건물번호로 기기 생성 후 건물에 매핑

--- src/main/java/org/morgorithm/frames/service/DeviceServiceImpl.java
+++ src/main/java/org/morgorithm/frames/service/DeviceServiceImpl.java
@@ -1,2 +1,59 @@
-package org.morgorithm.frames.service;public class DeviceServiceImpl {
+package org.morgorithm.frames.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.morgorithm.frames.entity.Device;
+import org.morgorithm.frames.entity.Facility;
+import org.morgorithm.frames.repository.DeviceRepository;
+import org.morgorithm.frames.repository.FacilityRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class DeviceServiceImpl implements DeviceService{
+    private DeviceRepository deviceRepository;
+    private FacilityRepository facilityRepository;
+
+    // 1. 건물 + 기기 등록
+    @Transactional
+    public Facility addFacility(UUID deviceId, String facilityName, Boolean state) {
+        Facility facility = Facility.builder()
+                .building(facilityName)
+                .build();
+
+        facility = facilityRepository.save(facility);
+
+        // 2. 기기 등록 (입구라고 가정)
+        Device device = Device.builder()
+                .deviceId(deviceId)
+                .bno(facility.getBno())
+                .state(state)
+                .build();
+
+        device = deviceRepository.save(device);
+        return facility;
+    }
+
+    // 3. 출구 기기 등록
+    @Transactional
+    public Device addDevice(UUID deviceId, Long bno, Boolean state) {
+        Device device = Device.builder()
+                .deviceId(deviceId)
+                .bno(bno)
+                .state(state)
+                .build();
+
+        device = deviceRepository.save(device);
+        return device;
+    }
+
+    @Transactional
+    public List<Device> getDevicesByFacility(Long bno) {
+        return deviceRepository.findByBno(bno);
+    }
 }
