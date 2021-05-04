9900117ea001c39f3e063c64e5c38581da0f8188
outstandingboy <outstanding1301@gmail.com>
2021-05-04 22:34:58 +0900

    feat: DeviceRepository 추가

--- src/main/java/org/morgorithm/frames/repository/DeviceRepository.java
+++ src/main/java/org/morgorithm/frames/repository/DeviceRepository.java
@@ -1,11 +1,10 @@
 package org.morgorithm.frames.repository;
 
-import org.morgorithm.frames.entity.Admin;
-import org.morgorithm.frames.entity.MemberImage;
+import org.morgorithm.frames.entity.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
-public interface AdminRepository extends JpaRepository<Admin,Long> {
-    Optional<Admin> findByUsername(String username);
+public interface DeviceRepository extends JpaRepository<Device,Long> {
+    List<Device> findByBno(Long bno);
 }
