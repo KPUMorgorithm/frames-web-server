7ba7589dfe3be8ddd9680e252ff0b1a5de9510f8
YoungKyonYou <man.spride1324@gmail.com>
2021-05-15 12:23:49 +0900

    build: build files

---
+++ build/generated/querydsl/org/morgorithm/frames/entity/QAdmin.java
@@ -0,0 +1,45 @@
+package org.morgorithm.frames.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QAdmin is a Querydsl query type for Admin
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QAdmin extends EntityPathBase<Admin> {
+
+    private static final long serialVersionUID = -1506481166L;
+
+    public static final QAdmin admin = new QAdmin("admin");
+
+    public final DateTimePath<java.time.LocalDateTime> createdDate = createDateTime("createdDate", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final DateTimePath<java.time.LocalDateTime> lastModifiedDate = createDateTime("lastModifiedDate", java.time.LocalDateTime.class);
+
+    public final StringPath password = createString("password");
+
+    public final StringPath username = createString("username");
+
+    public QAdmin(String variable) {
+        super(Admin.class, forVariable(variable));
+    }
+
+    public QAdmin(Path<? extends Admin> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QAdmin(PathMetadata metadata) {
+        super(Admin.class, metadata);
+    }
+
+}
+
