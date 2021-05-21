0d3821683dd82c37ff7d04d17dc386465aaec363
YoungKyonYou <man.spride1324@gmail.com>
2021-05-21 14:50:29 +0900

    feat" add dto for sms

---
+++ src/main/java/org/morgorithm/frames/dto/SmsDTO.java
@@ -0,0 +1,16 @@
+package org.morgorithm.frames.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SmsDTO {
+    String content;
+    String sender;
+    String receivedDate;
+}
