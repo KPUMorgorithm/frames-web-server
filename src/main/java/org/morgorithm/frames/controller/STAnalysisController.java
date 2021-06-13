5a5f57646153a51543ca37c8e4bb92eac8d7a841
outstandingboy <outstanding1301@gmail.com>
2021-06-13 19:25:33 +0900

    feat: 시공간연산 DTO, 페이지네이션 추가

--- src/main/java/org/morgorithm/frames/controller/STAnalysisController.java
+++ src/main/java/org/morgorithm/frames/controller/STAnalysisController.java
@@ -3,10 +3,15 @@ package org.morgorithm.frames.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.morgorithm.frames.dto.PageRequestDTO;
+import org.morgorithm.frames.dto.PageResultDTO;
 import org.morgorithm.frames.projection.AccessSet;
 import org.morgorithm.frames.service.AccessSetService;
 import org.morgorithm.frames.service.FacilityService;
 import org.morgorithm.frames.service.StatusService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,13 +29,18 @@ public class STAnalysisController {
         private final AccessSetService accessSetService;
 
     @GetMapping("/st")
-    public String getStAnalysis(Model model
-            , @RequestParam(value = "mno", required = false) Long mno){
-        List<AccessSet> accessSets = null;
-        accessSets = accessSetService.getAllAccessSet();
-//        if (mno != null) accessSetService.getAccessSetByMemberId(mno);
-//        else accessSets = accessSetService.getAllAccessSet();
+    public String getStAnalysis(Model model, PageRequestDTO pageRequestDTO){
+        List<AccessSet> accessSets = accessSetService.getAllAccessSet(pageRequestDTO);
+
+        Pageable pageable = pageRequestDTO.getPageable(Sort.by("regDate"));
+        final int start = (int)pageable.getOffset();
+        final int end = Math.min((start + pageable.getPageSize()), accessSets.size());
+
+        Page<AccessSet> page = new PageImpl<>(accessSets.subList(start, end), pageable, accessSets.size());
+        PageResultDTO<AccessSet, AccessSet> pageResultDTO = new PageResultDTO<>(page, e -> e);
         model.addAttribute("accessSets", accessSets);
+        model.addAttribute("result", pageResultDTO);
+        model.addAttribute("pageRequestDTO", pageRequestDTO);
         return "st/list";
     }
 }
