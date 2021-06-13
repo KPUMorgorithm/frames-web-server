f1658ef77c70ec69d92e7347e367a1bfc4a9550c
outstandingboy <outstanding1301@gmail.com>
2021-06-13 19:24:44 +0900

    feat: 시공간연산 관련 질의 추가

--- src/main/java/org/morgorithm/frames/repository/StatusRepository.java
+++ src/main/java/org/morgorithm/frames/repository/StatusRepository.java
@@ -3,6 +3,7 @@ package org.morgorithm.frames.repository;
 
 import org.morgorithm.frames.entity.Status;
 import org.morgorithm.frames.projection.AccessSet;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -123,9 +124,131 @@ public interface StatusRepository extends JpaRepository<Status,Long>, QuerydslPr
             "on m.mno = s1.member_mno " +
             "left join facility f " +
             "on f.bno = s1.facility_bno " +
-            "where s1.state = 0 and s1.member_mno = :memberId "  +
+            "where s1.state = 0 and s1.member_mno = :memberId " +
             "order by timeEnter asc", nativeQuery = true)
     List<AccessSet> getAccessSetByMemberId(@Param("memberId") Long memberId);
+
+    @Query(value = "select s1.member_mno memberId, m.name memberName, s1.facility_bno facilityId, f.building facilityName, s1.temperature temperatureEnter, s2.temperature temperatureLeave, s1.statusnum statusEnterId, s2.statusnum statusLeaveId, s1.regdate timeEnter, s2.regdate timeLeave " +
+            "from status s1 " +
+            "left join status s2 " +
+            "on s2.statusnum = (" +
+            "select s3.statusnum " +
+            "from status s3 " +
+            "where s1.facility_bno = s3.facility_bno " +
+            "and s1.member_mno = s3.member_mno " +
+            "and s1.state != s3.state " +
+            "order by abs(datediff(s1.regdate, s3.regdate)) asc " +
+            "limit 1 " +
+            ") " +
+            "left join member m " +
+            "on m.mno = s1.member_mno " +
+            "left join facility f " +
+            "on f.bno = s1.facility_bno " +
+            "where s1.state = 0 and s1.facility_bno = :facilityId " +
+            "order by timeEnter asc", nativeQuery = true)
+    List<AccessSet> getAccessSetByFacilityId(@Param("facilityId") Long facilityId);
+
+    @Query(value = "select s1.member_mno memberId, m.name memberName, s1.facility_bno facilityId, f.building facilityName, s1.temperature temperatureEnter, s2.temperature temperatureLeave, s1.statusnum statusEnterId, s2.statusnum statusLeaveId, s1.regdate timeEnter, s2.regdate timeLeave " +
+            "from status s1 " +
+            "left join status s2 " +
+            "on s2.statusnum = (" +
+            "select s3.statusnum " +
+            "from status s3 " +
+            "where s1.facility_bno = s3.facility_bno " +
+            "and s1.member_mno = s3.member_mno " +
+            "and s1.state != s3.state " +
+            "order by abs(datediff(s1.regdate, s3.regdate)) asc " +
+            "limit 1 " +
+            ") " +
+            "left join member m " +
+            "on m.mno = s1.member_mno " +
+            "left join facility f " +
+            "on f.bno = s1.facility_bno " +
+            "where s1.state = 0 and s1.member_mno = :memberId and s1.facility_bno = :facilityId " +
+            "order by timeEnter asc", nativeQuery = true)
+    List<AccessSet> getAccessSetByMemberIdAndFacilityId(@Param("memberId") Long memberId, @Param("facilityId") Long facilityId);
+
+
+    @Query(value = "select s1.member_mno memberId, m.name memberName, s1.facility_bno facilityId, f.building facilityName, s1.temperature temperatureEnter, s2.temperature temperatureLeave, s1.statusnum statusEnterId, s2.statusnum statusLeaveId, s1.regdate timeEnter, s2.regdate timeLeave " +
+            "from status s1 " +
+            "left join status s2 " +
+            "on s2.statusnum = (" +
+            "select s3.statusnum " +
+            "from status s3 " +
+            "where s1.facility_bno = s3.facility_bno " +
+            "and s1.member_mno = s3.member_mno " +
+            "and s1.state != s3.state " +
+            "order by abs(datediff(s1.regdate, s3.regdate)) asc " +
+            "limit 1 " +
+            ") " +
+            "left join member m " +
+            "on m.mno = s1.member_mno " +
+            "left join facility f " +
+            "on f.bno = s1.facility_bno " +
+            "where s1.state = 0 and (s1.regdate between :timeStart and :timeEnd or s2.regdate between :timeStart and :timeEnd) " +
+            "order by timeEnter asc", nativeQuery = true)
+    List<AccessSet> getAllAccessSet(String timeStart, String timeEnd);
+
+    @Query(value = "select s1.member_mno memberId, m.name memberName, s1.facility_bno facilityId, f.building facilityName, s1.temperature temperatureEnter, s2.temperature temperatureLeave, s1.statusnum statusEnterId, s2.statusnum statusLeaveId, s1.regdate timeEnter, s2.regdate timeLeave " +
+            "from status s1 " +
+            "left join status s2 " +
+            "on s2.statusnum = (" +
+            "select s3.statusnum " +
+            "from status s3 " +
+            "where s1.facility_bno = s3.facility_bno " +
+            "and s1.member_mno = s3.member_mno " +
+            "and s1.state != s3.state " +
+            "order by abs(datediff(s1.regdate, s3.regdate)) asc " +
+            "limit 1 " +
+            ") " +
+            "left join member m " +
+            "on m.mno = s1.member_mno " +
+            "left join facility f " +
+            "on f.bno = s1.facility_bno " +
+            "where s1.state = 0 and s1.member_mno = :memberId and (s1.regdate between :timeStart and :timeEnd or s2.regdate between :timeStart and :timeEnd) " +
+            "order by timeEnter asc", nativeQuery = true)
+    List<AccessSet> getAccessSetByMemberId(@Param("memberId") Long memberId, String timeStart, String timeEnd);
+
+
+    @Query(value = "select s1.member_mno memberId, m.name memberName, s1.facility_bno facilityId, f.building facilityName, s1.temperature temperatureEnter, s2.temperature temperatureLeave, s1.statusnum statusEnterId, s2.statusnum statusLeaveId, s1.regdate timeEnter, s2.regdate timeLeave " +
+            "from status s1 " +
+            "left join status s2 " +
+            "on s2.statusnum = (" +
+            "select s3.statusnum " +
+            "from status s3 " +
+            "where s1.facility_bno = s3.facility_bno " +
+            "and s1.member_mno = s3.member_mno " +
+            "and s1.state != s3.state " +
+            "order by abs(datediff(s1.regdate, s3.regdate)) asc " +
+            "limit 1 " +
+            ") " +
+            "left join member m " +
+            "on m.mno = s1.member_mno " +
+            "left join facility f " +
+            "on f.bno = s1.facility_bno " +
+            "where s1.state = 0 and s1.facility_bno = :facilityId and (s1.regdate between :timeStart and :timeEnd or s2.regdate between :timeStart and :timeEnd) " +
+            "order by timeEnter asc", nativeQuery = true)
+    List<AccessSet> getAccessSetByFacilityId(@Param("facilityId") Long facilityId, String timeStart, String timeEnd);
+
+    @Query(value = "select s1.member_mno memberId, m.name memberName, s1.facility_bno facilityId, f.building facilityName, s1.temperature temperatureEnter, s2.temperature temperatureLeave, s1.statusnum statusEnterId, s2.statusnum statusLeaveId, s1.regdate timeEnter, s2.regdate timeLeave " +
+            "from status s1 " +
+            "left join status s2 " +
+            "on s2.statusnum = (" +
+            "select s3.statusnum " +
+            "from status s3 " +
+            "where s1.facility_bno = s3.facility_bno " +
+            "and s1.member_mno = s3.member_mno " +
+            "and s1.state != s3.state " +
+            "order by abs(datediff(s1.regdate, s3.regdate)) asc " +
+            "limit 1 " +
+            ") " +
+            "left join member m " +
+            "on m.mno = s1.member_mno " +
+            "left join facility f " +
+            "on f.bno = s1.facility_bno " +
+            "where s1.state = 0 and s1.member_mno = :memberId and s1.facility_bno = :facilityId and (s1.regdate between :timeStart and :timeEnd or s2.regdate between :timeStart and :timeEnd) " +
+            "order by timeEnter asc", nativeQuery = true)
+    List<AccessSet> getAccessSetByMemberIdAndFacilityId(@Param("memberId") Long memberId, @Param("facilityId") Long facilityId, String timeStart, String timeEnd);
 
     @Query(value = "select * from status s where s.facility_bno = #{accessSet.facilityId} and s.regdate between #{accessSet.timeEnter} and #{accessSet.timeLeave}", nativeQuery = true)
     List<Object[]> getStatusOverlapped(@Param("accessSet") AccessSet accessSet); // TODO: 고쳐야함
