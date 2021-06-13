6601c0cdafbf2ee022830bcbb5119f676332063e
outstandingboy <outstanding1301@gmail.com>
2021-06-13 19:25:10 +0900

    feat: 시공간 연산 관련 질의 및 페이지네이션 추가

--- src/main/java/org/morgorithm/frames/service/AccessSetService.java
+++ src/main/java/org/morgorithm/frames/service/AccessSetService.java
@@ -1,13 +1,21 @@
 package org.morgorithm.frames.service;
 
+import com.querydsl.core.BooleanBuilder;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.morgorithm.frames.dto.PageRequestDTO;
+import org.morgorithm.frames.dto.PageResultDTO;
+import org.morgorithm.frames.dto.StatusDTO;
 import org.morgorithm.frames.entity.Status;
 import org.morgorithm.frames.projection.AccessSet;
 import org.morgorithm.frames.repository.StatusRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Service
 @Log4j2
@@ -16,12 +24,51 @@ public class AccessSetService {
     private StatusRepository statusRepository;
 
     // 1. 멤버의 시설 입장, 퇴장 기간 로그 (멤버id, 시설id, 입장로그번호, 퇴장로그번호, 입장시간, 퇴장시간)
-    public List<AccessSet> getAllAccessSet() {
-        return statusRepository.getAllAccessSet();
-    }
-
-    public List<AccessSet> getAccessSetByMemberId(Long memberId) {
-        return statusRepository.getAccessSetByMemberId(memberId);
+    public List<AccessSet> getAllAccessSet(PageRequestDTO requestDTO) {
+        if (requestDTO == null) {
+            return statusRepository.getAllAccessSet();
+        }
+        List<AccessSet> result = null;
+//        Pageable pageable = requestDTO.getPageable(Sort.by("regDate"));
+        if (requestDTO.getMno() != null && requestDTO.getMno().length() > 0) {
+            if (requestDTO.getFrom() != null && requestDTO.getFrom().length() > 0
+                    && requestDTO.getTo() != null  && requestDTO.getTo().length() > 0) {
+                if (requestDTO.getKeyword() != null && requestDTO.getKeyword().length() > 0) {
+                    result = statusRepository.getAccessSetByMemberIdAndFacilityId(Long.parseLong(requestDTO.getMno()), Long.parseLong(requestDTO.getKeyword()), requestDTO.getFrom(), requestDTO.getTo());
+                }
+                else {
+                    result = statusRepository.getAccessSetByMemberId(Long.parseLong(requestDTO.getMno()), requestDTO.getFrom(), requestDTO.getTo());
+                }
+            }
+            else {
+                if (requestDTO.getKeyword() != null && requestDTO.getKeyword().length() > 0) {
+                    result = statusRepository.getAccessSetByMemberIdAndFacilityId(Long.parseLong(requestDTO.getMno()), Long.parseLong(requestDTO.getKeyword()));
+                }
+                else {
+                    result = statusRepository.getAccessSetByMemberId(Long.parseLong(requestDTO.getMno()));
+                }
+            }
+        }
+        else {
+            if (requestDTO.getFrom() != null && requestDTO.getFrom().length() > 0
+                    && requestDTO.getTo() != null  && requestDTO.getTo().length() > 0) {
+                if (requestDTO.getKeyword() != null && requestDTO.getKeyword().length() > 0) {
+                    result = statusRepository.getAccessSetByFacilityId(Long.parseLong(requestDTO.getKeyword()), requestDTO.getFrom(), requestDTO.getTo());
+                }
+                else {
+                    result = statusRepository.getAllAccessSet(requestDTO.getFrom(), requestDTO.getTo());
+                }
+            }
+            else {
+                if (requestDTO.getKeyword() != null && requestDTO.getKeyword().length() > 0) {
+                    result = statusRepository.getAccessSetByFacilityId(Long.parseLong(requestDTO.getKeyword()));
+                }
+                else {
+                    result = statusRepository.getAllAccessSet();
+                }
+            }
+        }
+        return result;
     }
 
     // 2. 같은 공간에 있었던 멤버 목록 질의
