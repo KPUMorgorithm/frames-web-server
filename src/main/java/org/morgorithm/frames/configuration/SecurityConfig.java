dd7164b3e41d071e159a01423795d0235e28313a
outstandingboy <outstanding1301@gmail.com>
2021-06-17 21:58:29 +0900

    style: 불필요한 주석 제거
    antMatchers("/user/info").hasRole("MEMBER")

--- src/main/java/org/morgorithm/frames/configuration/SecurityConfig.java
+++ src/main/java/org/morgorithm/frames/configuration/SecurityConfig.java
@@ -32,7 +32,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                //                .antMatchers("/user/info").hasRole("MEMBER")
                 .antMatchers("/api/test").hasAnyRole("MEMBER", "ADMIN")
                 .antMatchers("/**").permitAll()
                 .and()
