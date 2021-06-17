b1a2d9d9abc9deef7dbfa34b877202f85462afe7
outstandingboy <outstanding1301@gmail.com>
2021-06-17 22:06:52 +0900

    style: 불필요한 주석 제거

--- src/main/java/org/morgorithm/frames/dto/MemberDTO.java
+++ src/main/java/org/morgorithm/frames/dto/MemberDTO.java
@@ -14,6 +14,6 @@ public class MemberDTO {
     private Long mno;
     private String name;
     private String phone;
-    @Builder.Default //디폴트로 초기화되어 있는 것
+    @Builder.Default
     private List<MemberImageDTO> imageDTOList = new ArrayList<>();
 }
