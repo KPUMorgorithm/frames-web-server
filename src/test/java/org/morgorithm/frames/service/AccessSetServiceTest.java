96260d6f09cd8e23baa3786a7f5f277780445045
outstandingboy <outstanding1301@gmail.com>
2021-06-17 21:44:01 +0900

    style: 불필요한 import 제거

--- src/test/java/org/morgorithm/frames/service/AccessSetServiceTest.java
+++ src/test/java/org/morgorithm/frames/service/AccessSetServiceTest.java
@@ -3,13 +3,10 @@ package org.morgorithm.frames.service;
 import org.junit.jupiter.api.Test;
 import org.morgorithm.frames.entity.Status;
 import org.morgorithm.frames.projection.AccessSet;
-import org.morgorithm.frames.repository.StatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AccessSetServiceTest {
