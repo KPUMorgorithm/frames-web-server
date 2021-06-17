1c4893b9f37fd3cd2021a23cc88b385cfb231ce8
outstandingboy <outstanding1301@gmail.com>
2021-06-17 22:08:36 +0900

    style: 불필요한 주석 제거

--- src/main/java/org/morgorithm/frames/projection/AccessSet.java
+++ src/main/java/org/morgorithm/frames/projection/AccessSet.java
@@ -2,38 +2,19 @@ package org.morgorithm.frames.projection;
 
 import java.time.LocalDateTime;
 
-// 멤버의 시설 입장, 퇴장 기간 로그 (멤버id, 시설id, 입장로그번호, 퇴장로그번호, 입장시간, 퇴장시간)
 //@AllArgsConstructor
 //@ToString
 //@Getter
 public interface AccessSet {
-//    private Long memberId;
-//    private Long facilityId;
-//    private Long statusEnterId;
-//    private Long statusLeaveId;
-//    private double temperatureEnter;
-//    private double temperatureLeave;
-//    private LocalDateTime timeEnter;
-//    private LocalDateTime timeLeave;
-
     Long getMemberId();
-
     String getMemberName();
-
     Long getFacilityId();
-
     String getFacilityName();
-
     Long getStatusEnterId();
-
     Long getStatusLeaveId();
-
     Double getTemperatureEnter();
-
     Double getTemperatureLeave();
-
     LocalDateTime getTimeEnter();
-
     LocalDateTime getTimeLeave();
 
     default String asString() {
