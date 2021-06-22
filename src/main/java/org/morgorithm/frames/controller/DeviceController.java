08ee784f3e2b3c8eeaae51fdc6d9bf148f6255d8
outstandingboy <outstanding1301@gmail.com>
2021-06-22 21:17:52 +0900

    style: 불필요한 import 제거

--- src/main/java/org/morgorithm/frames/controller/DeviceController.java
+++ src/main/java/org/morgorithm/frames/controller/DeviceController.java
@@ -1,17 +1,12 @@
 package org.morgorithm.frames.controller;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.morgorithm.frames.entity.Device;
 import org.morgorithm.frames.entity.Facility;
 import org.morgorithm.frames.service.DeviceService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/device")
