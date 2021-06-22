55f80d8059233506da320d6e98e9507698738b7c
outstandingboy <outstanding1301@gmail.com>
2021-06-22 21:17:15 +0900

    feat: Repository에 final 추가

--- src/main/java/org/morgorithm/frames/controller/DeviceController.java
+++ src/main/java/org/morgorithm/frames/controller/DeviceController.java
@@ -1,27 +1,35 @@
 package org.morgorithm.frames.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.morgorithm.frames.entity.Device;
+import org.morgorithm.frames.entity.Facility;
 import org.morgorithm.frames.service.DeviceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("/device")
 @RequiredArgsConstructor
 public class DeviceController {
-    private DeviceService deviceService;
+    private final DeviceService deviceService;
 
     @PostMapping(value = "/facility", produces = "text/plain")
     public String addFacility(@RequestParam String deviceId, @RequestParam String facilityName, @RequestParam Boolean state) {
-        return "" + deviceService.addFacility(UUID.fromString(deviceId), facilityName, state).getBno();
+        Facility facility = deviceService.addFacility(deviceId, facilityName, state);
+        Long bno = facility.getBno();
+        return "" + bno;
     }
 
     @PostMapping(value = "/device", produces = "text/plain")
     public String addDevice(@RequestParam String deviceId, @RequestParam Long bno, @RequestParam Boolean state) {
-        return "" + deviceService.addDevice(UUID.fromString(deviceId), bno, state).getBno();
+        Facility facility = deviceService.addDevice(deviceId, bno, state);
+        String bname = facility.getBuilding();
+        return "" + bname;
     }
 }
